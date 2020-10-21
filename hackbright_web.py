82ffe509d6c8c057982ce028c6be40e6fac9db36
georgiaschmitt <georgia.schmitt@gmail.com>
2020-10-21 22:13:33 +0000

    Finished student creation templates, working on connecting them
    Co-authored-by: Erin Grady <erinmariegrady@gmail.com>

--- hackbright_web.py
+++ hackbright_web.py
@@ -1,6 +1,6 @@
 """A web application for tracking projects, students, and student grades."""
 
-from flask import Flask, request, render_template
+from flask import Flask, redirect, request, render_template
 
 import hackbright
 
@@ -11,11 +11,41 @@ app = Flask(__name__)
 def get_student():
     """Show information about a student."""
 
-    github = "jhacks"
+    github = request.args.get('github')
 
     first, last, github = hackbright.get_student_by_github(github)
 
-    return "{} is the GitHub account for {} {}".format(github, first, last)
+    html = render_template("student_info.html", first=first,
+                            last=last, github=github)
+
+    return html
+    
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+@app.route("/new-student-form")
+def display_student_form():
+
+    return render_template("new_student_creation.html")
+
+@app.route("/new-student-creation", methods=["POST"])
+def create_new_student():
+    
+    first = request.form.get("first_name")
+    last = request.form.get("last_name")
+    github = request.form.get("github")
+    hackbright.make_new_student(first, last, github)
+
+    return render_template("/confirmation.html", github=github)
+
+# @app.route("/confirmation")
+# def display_confirmation():
+
+
+    # return render_template("confirmation.html")
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
